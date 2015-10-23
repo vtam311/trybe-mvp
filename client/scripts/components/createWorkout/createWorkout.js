5e1604641f2c3a3b08bdd5378e5ff5d7f88e3684
VINCE <vtam90@gmail.com>
2015-10-23 15:58:05 -0700

    (feat) Add component for UITableView and remove progress bar from feed

---
+++ client/scripts/components/createWorkout/createWorkout.js
@@ -0,0 +1,12 @@
+/*
+* @Author: vincetam
+* @Date:   2015-10-23 15:04:43
+* @Last Modified by:   vincetam
+* @Last Modified time: 2015-10-23 15:55:31
+*/
+
+'use strict';
+
+var React = require('react-native');
+var TableView = require('react-native-tableview');
+
