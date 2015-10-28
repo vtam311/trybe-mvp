62fce29edb5e4af2d4563a3371f5b451d67d34a8
VINCE <vtam90@gmail.com>
2015-10-28 15:29:25 -0700

    (feat) Add 'add part' and 'add exercise' buttons to createWorkout, without functionality

--- client/scripts/components/createWorkout/createWorkout.js
+++ client/scripts/components/createWorkout/createWorkout.js
@@ -1,21 +1,14 @@
 /*
 * @Author: vincetam
 * @Date:   2015-10-23 15:04:43
-* @Last Modified by:   vincetam
-* @Last Modified time: 2015-10-27 18:39:20
+* @Last Modified by:   VINCE
+* @Last Modified time: 2015-10-28 15:29:00
 */
 
 'use strict';
 
 var React = require('react-native');
 var createWorkoutStore = require('../../stores/createWorkoutStore');
-
-//Load components
-// var TableView = require('react-native-tableview');
-// var Section = TableView.Section;
-// var Item = TableView.Item;
-// var Cell = TableView.Cell;
-//
 
 var {
   StyleSheet,
@@ -24,8 +17,8 @@ var {
   View,
   TouchableHighlight,
   TextInput,
-  ActivityIndicatorIOS, //delete later
-  Image
+  Image,
+  Dimensions
 } = React;
 
 import {TableView, Section, Cell, CustomCell} from 'react-native-tableview-simple';
@@ -52,57 +45,70 @@ var ModifyWorkout = React.createClass({
   render: function(){
     return (
       /* jshint ignore:start */
-      <ScrollView contentContainerStyle={styles.stage}>
-        <TableView>
-          <Section>
-            <Cell cellstyle="RightDetail" accessory="DisclosureIndicator" title="Workout Date" detail="Today" />
-          </Section>
-          <Section header="Workout Details">
-            <Cell cellstyle="Subtitle" title="Instructions" detail="In 20 min, perform as many rounds as possible of" accessory="DisclosureIndicator"/>
-            <Cell cellstyle="Basic" accessory="DisclosureIndicator" title="5 Pull Ups" onPress={() => {console.log('Heyho!')}}/>
-            <Cell cellstyle="Basic" accessory="DisclosureIndicator" title="10 Push Ups" onPress={() => {console.log('Heyho!')}}/>
-            <Cell cellstyle="Basic" accessory="DisclosureIndicator" title="15 Squats" onPress={() => {console.log('Heyho!')}}/>
-            <CustomCell customHeight={44} onPress={() => {console.log('Heyho!')}}>
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.stage}>
+          <TableView>
+            <Section>
+              <CustomCell onPress={() => {console.log('Heyho!')}}>
+                <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                  <Text style={{fontSize: 16, color: '#9B9B9B'}}>Date</Text>
+                  <View style={{flexDirection: 'row'}}>
+                    <Text style={{fontSize: 16}}>Today</Text>
+                    <Image
+                      style={{height: 13, width: 8, marginTop: 4, marginLeft: 5}}
+                      source={require('image!disclosureIndicator')} />
+                  </View>
+                </View>
+              </CustomCell>
+            </Section>
+            <Section header="Part 1">
+              <CustomCell customHeight={70}>
+                <View style={{flex: 1, flexDirection: 'column', marginTop: 5}}>
+                  <Text style={{fontSize: 14, color: '#9B9B9B'}}>Instructions</Text>
+                  <TextInput
+                      style={{height: 40}}/>
+                </View>
+              </CustomCell>
+              <CustomCell onPress={() => {console.log('Heyho!')}}>
+                <Image
+                  style={{height: 14, width: 14, marginTop: 2, marginRight: 8}}
+                  source={require('image!clearButton')} />
+                <Text style={{flex: 1, fontSize: 16}}>5 Pull Ups</Text>
+                <Image
+                  style={{height: 13, width: 8}}
+                  source={require('image!disclosureIndicator')} />
+              </CustomCell>
+              <CustomCell onPress={() => {console.log('Heyho!')}}>
+                <Image
+                  style={{height: 14, width: 14, marginTop: 2, marginRight: 8}}
+                  source={require('image!addButton')} />
+                <Text style={{flex: 1, fontSize: 16}}>Add Exercise</Text>
+              </CustomCell>
+            </Section>
+            <View style={{flex: 1, flexDirection: 'row', marginLeft: 10}}>
               <Image
-                style={{height: 16, width: 16, marginRight: 5}}
+                style={{height: 14, width: 14, marginTop: 4, marginRight: 8}}
                 source={require('image!addButton')} />
-              <Text style={{flex: 1, fontSize: 16}}>Add Exercise</Text>
-            </CustomCell>
-          </Section>
-        </TableView>
-      </ScrollView>
+              <Text style={{flex: 1, fontSize: 16}}>Add Part</Text>
+            </View>
+          </TableView>
+        </ScrollView>
+      </View>
       /* jshint ignore:end */
     );
   }
 });
 
 var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EFEFF4',
+  },
   stage: {
     backgroundColor: '#EFEFF4',
     paddingTop: 20,
     paddingBottom: 20,
-  },
-  customCell: {
-    height: 100
   }
 });
 
 module.exports = ModifyWorkout;
-
-
-
-      //Below is TableView component using react-native-tableview
-        // <TableView style={{flex:1}}
-        //    tableViewStyle={TableView.Consts.Style.Grouped}
-        //    tableViewCellStyle={TableView.Consts.CellStyle.Value1}
-        //    onPress={(event) => console.log(event)}>
-        //   <Section arrow={true}>
-        //     <Item value="Today" detail="Today">Workout Date</Item>
-        //   </Section>
-        //   <Section>
-        //     <Item detail="In 20 min, perform as many rounds as possible of" arrow={true}>Instructions</Item>
-        //     <Item arrow={true}>5 Pull Ups</Item>
-        //     <Item arrow={true}>10 Push Ups</Item>
-        //     <Item arrow={true}>15 Squats</Item>
-        //   </Section>
-        // </TableView>
