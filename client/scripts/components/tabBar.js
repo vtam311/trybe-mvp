2a39dfea8b55312e28895bfc45415e085f234846
VINCE <vtam90@gmail.com>
2015-10-29 17:12:08 -0700

    (feat) Move tabBar logic to tabBar.js

---
+++ client/scripts/components/tabBar.js
@@ -0,0 +1,90 @@
+/*
+* @Author: vincetam
+* @Date:   2015-10-29 17:05:47
+* @Last Modified by:   vincetam
+* @Last Modified time: 2015-10-29 17:11:48
+*/
+
+'use strict';
+
+var React = require('react-native');
+var indexStore = require('../stores/indexStore');
+var indexActions = require('../actions/indexActions');
+
+//Load components
+var FeedTab = require('../components/feed/feed');
+var WorkoutTab = require('../components/workoutTab/workoutTab');
+var LogTab = require('../components/log/log');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TabBarIOS,
+  StatusBarIOS,
+  View,
+} = React;
+
+
+var TabBar = React.createClass({
+  getInitialState: function(){
+    return {
+      selectedTab: indexStore.getTab()
+    };
+  },
+  componentDidMount: function(){
+    indexStore.addChangeListener(this._onChange);
+    StatusBarIOS.setStyle(1);
+  },
+  componentWillUnmount: function(){
+    indexStore.removeChangeListener(this._onChange);
+  },
+  _onChange: function(){
+    this.setState({
+      selectedTab: indexStore.getTab()
+    });
+  },
+  changeTab: function(tabName) {
+    indexActions.setTab(tabName);
+  },
+
+  render: function() {
+    return (
+      /* jshint ignore:start */
+      <TabBarIOS>
+        <TabBarIOS.Item
+          title='Profile'
+          icon={ require('image!profile') }
+          onPress={ () => this.changeTab('profile') }
+          selected={ this.state.selectedTab === 'profile' }>
+          <LogTab/>
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          title='Home'
+          icon={ require('image!home') }
+          onPress={ () => this.changeTab('feed') }
+          selected={ this.state.selectedTab === 'feed' }>
+          <FeedTab/>
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          title='Workout'
+          icon={ require('image!workout') }
+          onPress={ () => this.changeTab('workout') }
+          selected={ this.state.selectedTab === 'workout' }>
+          <WorkoutTab/>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+      /* jshint ignore:end */
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  tabView: {
+    // fontFamily: 'Avenir'
+  }
+});
+
+module.exports = TabBar;
--- index.ios.js
+++ index.ios.js
@@ -3,13 +3,9 @@
 require("babel/register");
 
 var React = require('react-native');
-var indexStore = require('./client/scripts/stores/indexStore');
-var indexActions = require('./client/scripts/actions/indexActions');
 
 //Load components
-var FeedTab = require('./client/scripts/components/feed/feed');
-var WorkoutTab = require('./client/scripts/components/workoutTab/workoutTab');
-var LogTab = require('./client/scripts/components/log/log');
+var TabBar = require('./client/scripts/components/tabBar');
 
 var {
   AppRegistry,
@@ -24,55 +20,24 @@ var {
 var Trybe = React.createClass({
   getInitialState: function(){
     return {
-      selectedTab: indexStore.getTab()
     };
   },
   componentDidMount: function(){
-    indexStore.addChangeListener(this._onChange);
-    StatusBarIOS.setStyle(1);
   },
   componentWillUnmount: function(){
-    indexStore.removeChangeListener(this._onChange);
   },
   _onChange: function(){
-    this.setState({
-      selectedTab: indexStore.getTab()
-    });
-  },
-  changeTab: function(tabName) {
-    indexActions.setTab(tabName);
   },
 
   render: function() {
     return (
-      <TabBarIOS>
-        <TabBarIOS.Item
-          title='Profile'
-          icon={ require('image!profile') }
-          onPress={ () => this.changeTab('profile') }
-          selected={ this.state.selectedTab === 'profile' }>
-          <LogTab/>
-        </TabBarIOS.Item>
-
-        <TabBarIOS.Item
-          title='Home'
-          icon={ require('image!home') }
-          onPress={ () => this.changeTab('feed') }
-          selected={ this.state.selectedTab === 'feed' }>
-          <FeedTab/>
-        </TabBarIOS.Item>
-
-        <TabBarIOS.Item
-          title='Workout'
-          icon={ require('image!workout') }
-          onPress={ () => this.changeTab('workout') }
-          selected={ this.state.selectedTab === 'workout' }>
-          <WorkoutTab/>
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      /* jshint ignore:start */
+      <TabBar />
+      /* jshint ignore:end */
     );
   }
 });
+
 
 var styles = StyleSheet.create({
   tabView: {
