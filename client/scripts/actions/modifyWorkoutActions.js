d1e6a2287aa0eb1862aba71e21f2217d9d93a7fd
VINCE <vtam90@gmail.com>
2015-12-28 16:00:26 -0800

    (refactor) Comment out deprecated files

--- client/scripts/actions/modifyWorkoutActions.js
+++ client/scripts/actions/modifyWorkoutActions.js
@@ -1,99 +1,101 @@
-'use strict';
+//Deprecated as of 12/28/15
 
-var AppDispatcher = require('../dispatchers/AppDispatcher');
-var modifyWorkoutConstants = require('../constants/modifyWorkoutConstants');
+// 'use strict';
 
-var modifyWorkoutActions = {
-  getWorkout: function() {
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.GET_WORKOUT,
-      data: null
-    });
-  },
-  modifyWorkout: function(workout) {
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.MODIFY_WORKOUT,
-      data: workout
-    });
-  },
-  updateWorkout: function(workout) {
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.UPDATE_WORKOUT,
-      data: workout
-    });
-  },
-  // toggleTimeEdit: function() {
-  //   AppDispatcher.handleAction({
-  //     actionType: modifyWorkoutConstants.TOGGLE_TIME_EDIT,
-  //     data: null
-  //   });
-  // },
-  // toggleRepEdit: function(exerciseKey) {
-  //   AppDispatcher.handleAction({
-  //     actionType: modifyWorkoutConstants.TOGGLE_REP_EDIT,
-  //     data: exerciseKey
-  //   });
-  // },
-  setExerciseName: function(exName, partIdx, exIdx){
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.SET_EXERCISE_NAME,
-      data: {
-        exName: exName,
-        partIdx: partIdx,
-        exIdx: exIdx
-      }
-    });
-  },
-  setReps: function(reps, partIdx, exIdx) {
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.SET_REPS,
-      data: {
-        reps: reps,
-        partIdx: partIdx,
-        exIdx: exIdx
-      }
-    });
-  },
-  setLoad: function(load, partIdx, exIdx) {
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.SET_LOAD,
-      data: {
-        load: load,
-        partIdx: partIdx,
-        exIdx: exIdx
-      }
-    });
-  },
-  setHold: function(hold, partIdx, exIdx) {
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.SET_HOLD,
-      data: {
-        hold: hold,
-        partIdx: partIdx,
-        exIdx: exIdx
-      }
-    });
-  },
-  setDist: function(dist, partIdx, exIdx){
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.SET_DIST,
-      data: {
-        dist: dist,
-        partIdx: partIdx,
-        exIdx: exIdx
-      }
-    });
-  },
-  setDistUnit: function(unit, partIdx, exIdx){
-    AppDispatcher.handleAction({
-      actionType: modifyWorkoutConstants.SET_DISTUNIT,
-      data: {
-        unit: unit,
-        partIdx: partIdx,
-        exIdx: exIdx
-      }
-    });
-  }
-};
+// var AppDispatcher = require('../dispatchers/AppDispatcher');
+// var modifyWorkoutConstants = require('../constants/modifyWorkoutConstants');
 
-module.exports = modifyWorkoutActions;
+// var modifyWorkoutActions = {
+//   getWorkout: function() {
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.GET_WORKOUT,
+//       data: null
+//     });
+//   },
+//   modifyWorkout: function(workout) {
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.MODIFY_WORKOUT,
+//       data: workout
+//     });
+//   },
+//   updateWorkout: function(workout) {
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.UPDATE_WORKOUT,
+//       data: workout
+//     });
+//   },
+//   // toggleTimeEdit: function() {
+//   //   AppDispatcher.handleAction({
+//   //     actionType: modifyWorkoutConstants.TOGGLE_TIME_EDIT,
+//   //     data: null
+//   //   });
+//   // },
+//   // toggleRepEdit: function(exerciseKey) {
+//   //   AppDispatcher.handleAction({
+//   //     actionType: modifyWorkoutConstants.TOGGLE_REP_EDIT,
+//   //     data: exerciseKey
+//   //   });
+//   // },
+//   setExerciseName: function(exName, partIdx, exIdx){
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.SET_EXERCISE_NAME,
+//       data: {
+//         exName: exName,
+//         partIdx: partIdx,
+//         exIdx: exIdx
+//       }
+//     });
+//   },
+//   setReps: function(reps, partIdx, exIdx) {
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.SET_REPS,
+//       data: {
+//         reps: reps,
+//         partIdx: partIdx,
+//         exIdx: exIdx
+//       }
+//     });
+//   },
+//   setLoad: function(load, partIdx, exIdx) {
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.SET_LOAD,
+//       data: {
+//         load: load,
+//         partIdx: partIdx,
+//         exIdx: exIdx
+//       }
+//     });
+//   },
+//   setHold: function(hold, partIdx, exIdx) {
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.SET_HOLD,
+//       data: {
+//         hold: hold,
+//         partIdx: partIdx,
+//         exIdx: exIdx
+//       }
+//     });
+//   },
+//   setDist: function(dist, partIdx, exIdx){
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.SET_DIST,
+//       data: {
+//         dist: dist,
+//         partIdx: partIdx,
+//         exIdx: exIdx
+//       }
+//     });
+//   },
+//   setDistUnit: function(unit, partIdx, exIdx){
+//     AppDispatcher.handleAction({
+//       actionType: modifyWorkoutConstants.SET_DISTUNIT,
+//       data: {
+//         unit: unit,
+//         partIdx: partIdx,
+//         exIdx: exIdx
+//       }
+//     });
+//   }
+// };
+
+// module.exports = modifyWorkoutActions;
