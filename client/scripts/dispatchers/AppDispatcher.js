eb7c94254c6701da60c8a94b686aa458e5180259
VINCE <vtam90@gmail.com>
2016-02-03 13:10:39 -0800

    (fix) Fix dispatcher error where could not dispatch in the middle of a dispatch

--- client/scripts/dispatchers/AppDispatcher.js
+++ client/scripts/dispatchers/AppDispatcher.js
@@ -2,36 +2,19 @@
 * @Author: vincetam
 * @Date:   2015-07-30 10:33:49
 * @Last Modified by:   VINCE
-* @Last Modified time: 2016-01-29 18:05:26
+* @Last Modified time: 2016-02-03 13:09:50
 */
 
 'use strict';
 
 var Dispatcher = require('flux').Dispatcher;
-var assign = require('react/lib/Object.assign');
+var AppDispatcher = new Dispatcher();
 
-var AppDispatcher = assign(new Dispatcher(), {
-  handleAction: function(action){
-    this.dispatch({
-      source: 'VIEW_ACTION',
-      action: action
-    });
-  }
-});
+AppDispatcher.handleAction = function(action){
+  this.dispatch({
+    source: 'VIEW_ACTION',
+    action: action
+  });
+};
 
 module.exports = AppDispatcher;
-
-
-
-
-
-// var AppDispatcher = new Dispatcher();
-
-// AppDispatcher.handleAction = function(action){
-//   this.dispatch({
-//     source: 'VIEW_ACTION',
-//     action: action
-//   });
-// };
-
-// module.exports = AppDispatcher;
